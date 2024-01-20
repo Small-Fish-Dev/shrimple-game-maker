52b9e09c45baf11f6a787fa0b42eecb8dbd09ddb
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2024-01-20 04:29:12 +0300

    Function 'editor', first milestone

---
+++ code/UI/CodeBlockPanels/AbstractCodeBlockPanel.cs
@@ -0,0 +1,9 @@
+﻿using Sandbox.UI;
+using ShartCoding.ShartCode;
+
+namespace ShartCoding.UI.CodeBlockPanels;
+
+public abstract class AbstractCodeBlockPanel : Panel, ICodeBlockPanel
+{
+	public abstract ShartCodeBlock Block { get; set; }
+}
